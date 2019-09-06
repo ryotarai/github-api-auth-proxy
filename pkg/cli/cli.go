67a86bc395d9f0bd515ead5b8cdf4b46451f3bea
Ryota Arai <ryota.arai@gmail.com>
2019-09-06 23:55:24 +0900

    -bcrypt for generating bcrypt-ed password

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -2,12 +2,16 @@ package cli
 
 import (
 	"flag"
+	"fmt"
+	"github.com/ryotarai/github-api-auth-proxy/pkg/authz"
 	"github.com/ryotarai/github-api-auth-proxy/pkg/config"
 	"github.com/ryotarai/github-api-auth-proxy/pkg/handler"
-	"github.com/ryotarai/github-api-auth-proxy/pkg/authz"
+	"golang.org/x/crypto/ssh/terminal"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 	"net/url"
+	"syscall"
 )
 
 type CLI struct {
@@ -21,10 +25,16 @@ func (c *CLI) Start(args []string) error {
 	fs := flag.NewFlagSet(args[0], flag.ContinueOnError)
 	listen := fs.String("listen", ":8080", "")
 	configPath := fs.String("config", "", "")
+	bcryptMode := fs.Bool("bcrypt", false, "")
 
 	err := fs.Parse(args[1:])
 	if err != nil {
 		return err
+	}
+
+	if *bcryptMode {
+		generateBcrypt()
+		return nil
 	}
 
 	cfg, err := config.LoadYAMLFile(*configPath)
@@ -55,5 +65,21 @@ func (c *CLI) Start(args []string) error {
 		return err
 	}
 
+	return nil
+}
+
+func generateBcrypt() error {
+	fmt.Print("Password: ")
+	password, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return err
+	}
+
+	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("\nBcrypted: %s\n", string(b))
 	return nil
 }
